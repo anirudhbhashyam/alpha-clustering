18222b47808c7ae1bf6b8cba6283a28a237ff9ab
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2022-11-11 11:13:49 +0100

    Added docs.

--- src/alpha_clustering/cluster.py
+++ src/alpha_clustering/cluster.py
@@ -1,6 +1,8 @@
 import collections
 from dataclasses import dataclass, field
 
+from typing import Iterator
+
 import trimesh
 
 import networkx as nx
@@ -14,11 +16,13 @@ from .logger import Logger
 from .exceptions import NotFittedError
 
 LOGGER = Logger(__name__)
-LOGGER.set_level("DEBUG")
 
 @dataclass
 class Cluster:
-    shape: tuple[np.ndarray]
+    """
+    Custom interface to cluster data using an Alpha shape.
+    """
+    shape: list[np.ndarray]
 
     _adjacency_list: dict[int, set[int]] = field(
         default_factory = lambda: collections.defaultdict(set)
@@ -29,6 +33,9 @@ class Cluster:
     )
     
     def fit(self) -> None:
+        """
+        Constructs an intermediary data structure from the Alpha shape.
+        """
         LOGGER.info("Fitting the cluster model...")
         if isinstance(self.shape, trimesh.Trimesh):
             edges = trimesh.geometry.faces_to_edges(self.shape.faces)
@@ -39,7 +46,20 @@ class Cluster:
             self._adjacency_list[v1].add(v2)
             self._adjacency_list[v2].add(v1)
                         
-    def predict_iter(self, cluster_threshold: int) -> set:
+    def predict_iter(self, cluster_threshold: int) -> Iterator[set[int]]:
+        """
+        Predicts clusters using a connected component search.
+
+        Parameters
+        ----------
+        cluster_threshold: 
+            The minimum number of points that should be present for a set to be considered a cluster.
+
+        Returns
+        -------
+        ``Iterator[set[int]]``:
+            Iterable that yields clusters.
+        """
         LOGGER.info("Predicting the clusters using the \u03B1-shape...(iterating)")
         if len(self._adjacency_list) == 0:
             raise NotFittedError(
@@ -55,6 +75,19 @@ class Cluster:
                     yield component
 
     def predict(self, cluster_threshold: int = 2) -> list[list[int]]:
+        """
+        Predicts clusters using a connected component search.
+
+        Parameters
+        ----------
+        cluster_threshold: 
+            The minimum number of points that should be present for a set to be considered a cluster.
+
+        Returns
+        -------
+        ``list[list[int]]``:
+            A list of clusters. Each sublist is a cluster containing the indices of the vertices.
+        """
         LOGGER.info("Predicting the clusters using the \u03B1-shape...")
         # if len(self._adjacency_list) == 0:
         #     raise NotFittedError(
@@ -70,14 +103,27 @@ class Cluster:
                     self.clusters.append(component)
         return self.clusters
 
-    def _find_communities(self) -> list[set[int]]:
-        LOGGER.info("Finding the communities...")
-        graph = nx.Graph(self._adjacency_list)
-        communities = list(nx.algorithms.community.greedy_modularity_communities(graph))
-        m = nx.algorithms.community.modularity(graph, communities)
-        return m, communities
+    # def _find_communities(self) -> list[set[int]]:
+    #     LOGGER.info("Finding the communities...")
+    #     graph = nx.Graph(self._adjacency_list)
+    #     communities = list(nx.algorithms.community.greedy_modularity_communities(graph))
+    #     m = nx.algorithms.community.modularity(graph, communities)
+    #     return m, communities
         
     def _bfs(self, source: float) -> set:
+        """
+        A private function to perform a breadth first search used by `self.predict`.
+
+        Parameters
+        ----------
+        source: 
+            A node in the graph from where a bfs should be done.
+
+        Returns
+        -------
+        set:
+            The visited nodes from the source after a bfs is conducted.
+        """
         visited = set()
         queue = {source}
         while queue:
@@ -96,6 +142,18 @@ class Cluster:
         other_cluster_data: list[tuple[str, int]],
         threshold: int
     ) -> pd.DataFrame:
+        """
+        A convenience method that is used to summarise a clustering task.
+        
+        Parameters
+        ----------
+        dataset: 
+            The name of the dataset that is has been clustered.
+        other_cluster_data: 
+            Key value pairs of data of any other clustering method that has been used.
+        threshold:
+            The clustering threshold used for alpha clustering.
+        """
         other_data_dict = {
             s: n for s, n in other_cluster_data
         }
@@ -112,6 +170,9 @@ class Cluster:
 
 @dataclass
 class ClusterEvaluate:
+    """
+    A simple class that evaluates cluster labels using different metrics.
+    """
     data: np.ndarray
     true_clusters: np.array
     predicted_clusters: np.array
@@ -123,6 +184,14 @@ class ClusterEvaluate:
     davies_bouldin_score: float = -100
 
     def _external_evaluation(self, method: str) -> None:
+        """
+        Performs an external evaluation of cluster labels. The external evaluation will utilise the ground truth clusters.
+
+        Parameters
+        ----------
+        method:
+            The clustering method used.
+        """
         LOGGER.info(f"Evaluating the clusters predicted by method {method} using external data...")
         self.rand_score = metrics.rand_score(self.true_clusters, self.predicted_clusters)
         self.adjusted_rand_score = metrics.adjusted_rand_score(self.true_clusters, self.predicted_clusters)
@@ -130,11 +199,32 @@ class ClusterEvaluate:
         self.homogeneity_score = metrics.homogeneity_score(self.true_clusters, self.predicted_clusters)
 
     def _internal_evaluation(self, method: str) -> None:
+        """
+        Performs an internal evaluation of cluster labels.
+
+        Parameters
+        ----------
+        method:
+            The clustering method used.
+        """
         LOGGER.info(f"Evaluating the clusters predicted by method {method} using internal metrics...")
         self.silhouette_score = metrics.silhouette_score(self.data, self.predicted_clusters)
         self.davies_bouldin_score = metrics.davies_bouldin_score(self.data, self.predicted_clusters)
 
     def get_results(self, method: str) -> pd.DataFrame:
+        """
+        Summarises the results from both types of evaluations.
+
+        Parameters
+        ----------
+        method:
+            The clustering method to which alpha clustering is compared to.
+
+        Returns
+        -------
+        ``pandas DataFrame``:
+            A dataframe containing all evaluation results.
+        """
         LOGGER.info("Extracting the cluster evaluation results...")
         self._external_evaluation(method)
         self._internal_evaluation(method)
